b91d6e3bcb33720389672dfc3aa008f92739659d
edomg <edomgezahegn54@gmail.com>
2026-05-07 15:46:52 -0500

    I set the properties for my hotel class

---
+++ src/main/java/Hotel.java
@@ -0,0 +1,9 @@
+  public class Hotel {
+
+    private String name;
+    private int numberOfSuites;
+    private int numberOfRooms;
+    private int bookedSuites;
+    private int bookedBasicRooms;
+
+}
