2714118c050ea016f61fe0197c2d2097be414997
edomg <edomgezahegn54@gmail.com>
2026-05-06 22:25:09 -0500

    I Added total time method and I Added total pay method

--- src/main/java/com/pluralsight/Employee.java
+++ src/main/java/com/pluralsight/Employee.java
@@ -1,4 +1,43 @@
 package com.pluralsight;
 
 public class Employee {
+    private int employeeId;
+    private String name;
+    private String department;
+    private double payRate;
+    private double hoursWorked;
+
+    public Employee(int employeeId, String name, String department, double payRate, double hoursWorked) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.department = department;
+        this.payRate = payRate;
+        this.hoursWorked = hoursWorked;
+    }
+
+    public double getRegularHours() {
+        if (hoursWorked > 40) {
+            return 40;
+        } else {
+            return hoursWorked;
+        }
+    }
+
+    public double getOvertimeHours() {
+        if (hoursWorked > 40) {
+            return hoursWorked - 40;
+        } else {
+            return 0;
+        }
+    }
+
+    public double getTotalPay() {
+        double regularPay = getRegularHours() * payRate;
+        double overtimePay = getOvertimeHours() * payRate * 1.5;
+
+        return regularPay + overtimePay;
+    }
 }
+
+
+
--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -3,4 +3,33 @@ package com.pluralsight;
 public class Main {
     public static void main(String[] args) {
 
-    }}
+        Room room1 = new Room(2, 150.00, false, false);
+
+        System.out.println("ROOM TEST");
+        System.out.println("Number of beds: " + room1.getNumberOfBeds());
+        System.out.println("Room price: $" + room1.getPrice());
+        System.out.println("Occupied: " + room1.isOccupied());
+        System.out.println("Dirty: " + room1.isDirty());
+        System.out.println("Available: " + room1.isAvailable());
+
+        System.out.println();
+
+        Reservation reservation1 = new Reservation("king", 3, true);
+
+        System.out.println("RESERVATION TEST");
+        System.out.println("Room type: " + reservation1.getRoomType());
+        System.out.println("Price per night: $" + reservation1.getPrice());
+        System.out.println("Number of nights: " + reservation1.getNumberOfNights());
+        System.out.println("Weekend: " + reservation1.isWeekend());
+        System.out.println("Reservation total: $" + reservation1.getReservationTotal());
+
+        System.out.println();
+
+        Employee employee1 = new Employee(101, "Sara", "Front Desk", 20.00, 45);
+
+        System.out.println("EMPLOYEE TEST");
+        System.out.println("Regular hours: " + employee1.getRegularHours());
+        System.out.println("Overtime hours: " + employee1.getOvertimeHours());
+        System.out.println("Total pay: $" + employee1.getTotalPay());
+    }
+}
