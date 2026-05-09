c381785e35503f4ab45ca41d80748c527eb60e20
edomg <edomgezahegn54@gmail.com>
2026-05-09 13:01:51 -0500

    created Hotel constructor and completed Exercise 3 classes"

--- src/main/java/Hotel.java
+++
@@ -1,9 +0,0 @@
-  public class Hotel {
-
-    private String name;
-    private int numberOfSuites;
-    private int numberOfRooms;
-    private int bookedSuites;
-    private int bookedBasicRooms;
-
-}
