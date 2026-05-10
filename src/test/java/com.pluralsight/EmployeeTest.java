84f0276312ca0cf0e5b5e41b6009ad9d539d4357
edomg <edomgezahegn54@gmail.com>
2026-05-10 17:47:40 -0500

    I created EmployeeTest.java class and Added EmployeeTest for punchIn method

---
+++ src/test/java/com.pluralsight/EmployeeTest.java
@@ -0,0 +1,20 @@
+package com.pluralsight;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class EmployeeTest {
+
+    @Test
+    public void testPunchIn() {
+
+        Employee employee = new Employee(101, "Sara", "Front Desk", 20.00, 0);
+
+        employee.punchIn(900);
+
+        assertEquals(900, employee.getPunchInTime());
+    }
+
+}
