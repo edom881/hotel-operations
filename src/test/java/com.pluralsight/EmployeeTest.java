72128cc66a200c1594604890268796d4b78fec7e
edomg <edomgezahegn54@gmail.com>
2026-05-10 17:50:41 -0500

    Added EmployeeTest for punchIn method and Completed JUnit tests for Room and Employee classes

--- src/test/java/com.pluralsight/EmployeeTest.java
+++ src/test/java/com.pluralsight/EmployeeTest.java
@@ -16,5 +16,15 @@ public class EmployeeTest {
 
         assertEquals(900, employee.getPunchInTime());
     }
+    @Test
+    public void testPunchOut() {
+
+        Employee employee = new Employee(101, "Sara", "Front Desk", 20.00, 0);
+
+        employee.punchIn(900);
+        employee.punchOut(1700);
+
+        assertEquals(1700, employee.getPunchOutTime());
+    }
 
 }
