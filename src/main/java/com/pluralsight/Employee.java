1acf4d2f3781eedd69abf6a879c3b338fa5ff94e
edomg <edomgezahegn54@gmail.com>
2026-05-09 22:21:09 -0500

    I created the punchIn and punchout variables and methods

--- src/main/java/com/pluralsight/Employee.java
+++ src/main/java/com/pluralsight/Employee.java
@@ -7,37 +7,51 @@ public class Employee {
     private double payRate;
     private double hoursWorked;
 
+    private int punchInTime;
+    private int punchOutTime;
+
     public Employee(int employeeId, String name, String department, double payRate, double hoursWorked) {
         this.employeeId = employeeId;
         this.name = name;
         this.department = department;
         this.payRate = payRate;
         this.hoursWorked = hoursWorked;
+
+    }
+    public void punchIn(int time) {
+        punchInTime = time;
     }
 
-    public double getRegularHours() {
-        if (hoursWorked > 40) {
-            return 40;
-        } else {
-            return hoursWorked;
+    public void punchOut(int time) {
+        punchOutTime = time;
+        hoursWorked = (punchOutTime - punchInTime) / 100.0;
+
+    }
+
+
+        public double getRegularHours () {
+            if (hoursWorked > 40) {
+                return 40;
+            } else {
+                return hoursWorked;
+            }
+        }
+
+        public double getOvertimeHours () {
+            if (hoursWorked > 40) {
+                return hoursWorked - 40;
+            } else {
+                return 0;
+            }
+        }
+
+        public double getTotalPay () {
+            double regularPay = getRegularHours() * payRate;
+            double overtimePay = getOvertimeHours() * payRate * 1.5;
+
+            return regularPay + overtimePay;
         }
     }
-
-    public double getOvertimeHours() {
-        if (hoursWorked > 40) {
-            return hoursWorked - 40;
-        } else {
-            return 0;
-        }
-    }
-
-    public double getTotalPay() {
-        double regularPay = getRegularHours() * payRate;
-        double overtimePay = getOvertimeHours() * payRate * 1.5;
-
-        return regularPay + overtimePay;
-    }
-}
 
 
 
